bca4677937b9714354c8f43b00e19fd9529c97ae
vijaysaiy <vijaysaiyekbote@gmail.com>
2021-11-28 21:11:37 +0530

    practice questions

--- src/com/vijay/practice/LeapYear.java
+++ src/com/vijay/practice/LeapYear.java
@@ -7,21 +7,6 @@ public class LeapYear {
         Scanner input = new Scanner(System.in);
         System.out.print("Enter year: ");
         int year = input.nextInt();
-//        if(year % 4 == 0){
-//            if(year % 100 == 0){
-//                if(year % 400 == 0){
-//                    System.out.println("Leap year");
-//                }
-//                else{
-//                    System.out.println("Not a leap year");
-//                }
-//            }else{
-//                System.out.println("Leap year");
-//            }
-//        }
-//        else{
-//            System.out.println("Not a leap year");
-//        }
         if(year % 4 == 0 || year % 400 == 0 && year % 100 != 0){
             System.out.println("Leap year");
         }else{
