3c4d9693d3ec76e1329341804e783d47ce691bcc
vijaysaiy <vijaysaiyekbote@gmail.com>
2022-01-08 19:13:48 +0530

    arrays

--- src/com/vijay/DS/Arrays/Basics.java
+++ src/com/vijay/DS/Arrays/Basics.java
@@ -1,2 +1,25 @@
-package com.vijay.DS.Arrays;public class Basics {
+package com.vijay.DS.Arrays;
+
+public class Basics {
+    public static void main(String[] args) {
+        /*
+        -----------------syntax-----------
+        datatype[] variable_name = new datatype[size];
+         */
+
+        //Q:Store 5 roll numbers
+
+        int[] rnos = new int[5];
+
+        //or directly
+        int[] rnos2 = {23,24,25,26,27};
+
+
+        //String arrays
+
+        String[] str = new String[5];
+        //each element of array is an object and has its own memory in heap
+        //each element has its ref variable pointing in heap and has default value of null
+
+    }
 }
