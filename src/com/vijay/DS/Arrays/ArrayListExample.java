4249e0c5fe0ab8d0188dceb894d27dc21e4ab404
vijaysaiy <vijaysaiyekbote@gmail.com>
2022-01-08 19:13:48 +0530

    arrays

--- src/com/vijay/DS/Arrays/ArrayListExample.java
+++ src/com/vijay/DS/Arrays/ArrayListExample.java
@@ -1,2 +1,36 @@
-package com.vijay.DS.Arrays;public class ArrayListExample {
+package com.vijay.DS.Arrays;
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class ArrayListExample {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        ArrayList<Integer> list = new ArrayList<>(5);
+        //manually adding elements
+//        list.add(67);
+//        list.add(59);
+//        list.add(5);
+//        list.add(2);
+//        list.add(20);
+//        list.add(21);
+//        list.add(22);
+//        list.add(23);
+//        list.add(24);
+//        list.add(25);
+//
+//        System.out.println(list.contains(2));
+//        list.set(0,99);  //updates 0th index with 99
+//        list.remove(2); //removes the element at index 2
+//        System.out.println(list);
+
+        //input
+        for(int i = 0;i < 5; i++){
+            list.add(in.nextInt());
+        }
+        //get item at any index
+        for(int i = 0;i < 5; i++){
+            System.out.print(list.get(i) + " "); //pass index here,list[index] won't work
+        }
+    }
 }
--- src/com/vijay/DS/Arrays/Multidimensional.java
+++ src/com/vijay/DS/Arrays/Multidimensional.java
@@ -1,2 +1,48 @@
-package com.vijay.DS.Arrays;public class Multidimensional {
+package com.vijay.DS.Arrays;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Multidimensional {
+    public static void main(String[] args) {
+        /*
+             1 2 3
+             4 5 6
+             7 8 9
+         */
+        Scanner in = new Scanner(System.in);
+        int[][] arr = new int[3][2];
+        //or directly
+        int[][] arr1 = {
+                {1,2,3},
+                {4,5,6},
+                {7,8,9}
+        };
+        //a 2d array can also be as
+        int[][] arr2 ={
+                {1,2,3},
+                {4,5},
+                {6,7,8,9}
+        };
+
+        //input
+        for(int row = 0; row < arr.length;row++){
+            for(int col = 0; col < arr[row].length;col++){
+                arr[row][col] = in.nextInt();
+            }
+        }
+        //output
+        for(int row = 0; row < arr.length;row++){
+            for(int col = 0; col < arr[row].length;col++){
+                System.out.print(arr[row][col] + " ");
+            }
+            System.out.println();
+        }
+        //output using enhanced for loop
+
+        for(int[] element : arr){
+            System.out.println(Arrays.toString(element));
+        }
+
+    }
 }
